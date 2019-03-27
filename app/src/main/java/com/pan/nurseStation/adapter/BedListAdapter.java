6c95ae58beee41ceb54ee3a67e0f947ea1ed0a35
ichieh <ichieh@foxmail.com>
2019-03-27 16:34:17 +0800

    优化adapter代码

--- app/src/main/java/com/pan/nurseStation/adapter/BedListAdapter.java
+++ app/src/main/java/com/pan/nurseStation/adapter/BedListAdapter.java
@@ -50,56 +50,31 @@ public class BedListAdapter extends BaseAdapter {
         LinearLayout linearLayout;
         if (convertView == null) {
             linearLayout = (LinearLayout) View.inflate(mContext, R.layout.item_simple_house, null);
-            // 这里做数据的绑定
-            TextView bedNo = linearLayout.findViewById(R.id.bed_id);
-            TextView patientName = linearLayout.findViewById(R.id.patient_name);
-            ImageView patientSex = linearLayout.findViewById(R.id.patient_sex);
-            TextView patientAge = linearLayout.findViewById(R.id.patient_age);
-            TextView patientNumber = linearLayout.findViewById(R.id.patient_number);
-
-            BedListResponseBean.PatientInfo patientInfo = mList.get(position);
-            bedNo.setText(patientInfo.getBed_id());
-            GradientDrawable myGrad = (GradientDrawable) bedNo.getBackground();
-            if (DBHisBusiness.bedTypeColorMap.containsKey(patientInfo.getLevel())) {
-                myGrad.setColor(DBHisBusiness.bedTypeColorMap.get(patientInfo.getLevel()));
-            }
-
-            patientName.setText(patientInfo.getName());
-            if (Objects.equals(patientInfo.getSex(), mContext.getString(R.string.sex_type_male))) {
-                patientSex.setBackground(mContext.getResources().getDrawable(R.drawable.ic_male));
-            } else if (Objects.equals(patientInfo.getSex(), mContext.getString(R.string.sex_type_female))) {
-                patientSex.setBackground(mContext.getResources().getDrawable(R.drawable.ic_female));
-            }
-            patientAge.setText(patientInfo.getAge());
-            patientNumber.setText(patientInfo.getHos_number());
-            Log.d(TAG, "getView: 新创建组件position=" + position);
         } else {
             linearLayout = (LinearLayout) convertView;
-            TextView bedNo = linearLayout.findViewById(R.id.bed_id);
-            TextView patientName = linearLayout.findViewById(R.id.patient_name);
-            ImageView patientSex = linearLayout.findViewById(R.id.patient_sex);
-            TextView patientAge = linearLayout.findViewById(R.id.patient_age);
-            TextView patientNumber = linearLayout.findViewById(R.id.patient_number);
-
-            BedListResponseBean.PatientInfo patientInfo = mList.get(position);
-            bedNo.setText(patientInfo.getBed_id());
-            GradientDrawable myGrad = (GradientDrawable) bedNo.getBackground();
-            if (DBHisBusiness.bedTypeColorMap.containsKey(patientInfo.getLevel())) {
-                myGrad.setColor(DBHisBusiness.bedTypeColorMap.get(patientInfo.getLevel()));
-            }
-
-            patientName.setText(patientInfo.getName());
-            if (Objects.equals(patientInfo.getSex(), mContext.getString(R.string.sex_type_male))) {
-                patientSex.setBackground(mContext.getResources().getDrawable(R.drawable.ic_male));
-            } else if (Objects.equals(patientInfo.getSex(), mContext.getString(R.string.sex_type_female))) {
-                patientSex.setBackground(mContext.getResources().getDrawable(R.drawable.ic_female));
-            }
-            patientAge.setText(patientInfo.getAge());
-            patientNumber.setText(patientInfo.getHos_number());
-
-            Log.d(TAG, "getView: 复用组件position=" + position);
-            Log.d(TAG, "getView: bedNo.getText()=" + bedNo.getText());
         }
+
+        TextView bedNo = linearLayout.findViewById(R.id.bed_id);
+        TextView patientName = linearLayout.findViewById(R.id.patient_name);
+        ImageView patientSex = linearLayout.findViewById(R.id.patient_sex);
+        TextView patientAge = linearLayout.findViewById(R.id.patient_age);
+        TextView patientNumber = linearLayout.findViewById(R.id.patient_number);
+
+        BedListResponseBean.PatientInfo patientInfo = mList.get(position);
+        bedNo.setText(patientInfo.getBed_id());
+        GradientDrawable myGrad = (GradientDrawable) bedNo.getBackground();
+        if (DBHisBusiness.bedTypeColorMap.containsKey(patientInfo.getLevel())) {
+            myGrad.setColor(DBHisBusiness.bedTypeColorMap.get(patientInfo.getLevel()));
+        }
+
+        patientName.setText(patientInfo.getName());
+        if (Objects.equals(patientInfo.getSex(), mContext.getString(R.string.sex_type_male))) {
+            patientSex.setBackground(mContext.getResources().getDrawable(R.drawable.ic_male));
+        } else if (Objects.equals(patientInfo.getSex(), mContext.getString(R.string.sex_type_female))) {
+            patientSex.setBackground(mContext.getResources().getDrawable(R.drawable.ic_female));
+        }
+        patientAge.setText(patientInfo.getAge());
+        patientNumber.setText(patientInfo.getHos_number());
 
         return linearLayout;
     }
