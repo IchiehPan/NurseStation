16a68aa2b94d8473e41596709326ecf94e9408d6
ichieh <ichieh@foxmail.com>
2019-03-29 17:31:53 +0800

    BeanKit工具包修改

--- javalib/src/main/java/com/pan/lib/util/BeanKit.java
+++ javalib/src/main/java/com/pan/lib/util/BeanKit.java
@@ -18,7 +18,17 @@ public class BeanKit {
      * 从一个JSON 对象字符格式中得到一个java对象
      */
     public static <T> T string2Bean(String json, Class<T> beanClass) {
-        return JSON.parseObject(json, beanClass);// 默认为UTF-8
+        T object = JSON.parseObject(json, beanClass);
+        if (object == null) {
+            try {
+                return (T) beanClass.getClass().newInstance();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return object;// 默认为UTF-8
     }
 
     /**
