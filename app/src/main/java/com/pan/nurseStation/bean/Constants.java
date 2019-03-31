c54e577234ef059271cdbce4e2eae589b03829b0
ichieh <ichieh@foxmail.com>
2019-03-31 15:10:54 +0800

    修正网页对应

--- app/src/main/java/com/pan/nurseStation/bean/Constants.java
+++ app/src/main/java/com/pan/nurseStation/bean/Constants.java
@@ -13,8 +13,8 @@ public class Constants {
     //    public final static String URL = "http://api.xiezn.com/dbhospital/?service=Login.index&number=011012&password=pwd";
 
     public final static String PATIENT_INFO_URL = "http://192.168.10.71:8689/index/ydhl/baseinfo/hos_number/";
-    public final static String MEDICAL_INFO_URL = "http://192.168.10.71:8689/index/ydhl/doctorenjoininfo/hos_number/";
-    public final static String MEDICAL_ORDER_URL = "http://192.168.10.71:8689/index/ydhl/enjoinInfo /hos_number/";
+    public final static String MEDICAL_INFO_URL = "http://192.168.10.71:8689/index/ydhl/enjoinInfo /hos_number/";
+    public final static String MEDICAL_ORDER_URL = "http://192.168.10.71:8689/index/ydhl/doctorenjoininfo/hos_number/";
     public final static String VITAL_SIGN_URL = "http://192.168.10.71:8689/index/ydhl/physicalRecord/hos_number/";
 
     /**
