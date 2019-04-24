a331010e8c80d5c1d8bcd39da81170eb46dc6378
ichieh <ichieh@foxmail.com>
2019-04-24 11:48:21 +0800

    修改上传规则

--- app/src/main/java/com/pan/nurseStation/MyApplication.java
+++ app/src/main/java/com/pan/nurseStation/MyApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.android.volley.toolbox.Volley;
 import com.pan.nurseStation.business.DBHisBusiness;
-import com.umeng.analytics.MobclickAgent;
 import com.umeng.commonsdk.UMConfigure;
 
 public class MyApplication extends Application {
@@ -21,7 +20,6 @@ public class MyApplication extends Application {
 //        参数2:设备类型，必须参数，传参数为UMConfigure.DEVICE_TYPE_PHONE则表示手机；传参数为UMConfigure.DEVICE_TYPE_BOX则表示盒子；默认为手机。
 //        参数3:Push推送业务的secret，需要集成Push功能时必须传入Push的secret，否则传空。
         UMConfigure.init(this, UMConfigure.DEVICE_TYPE_PHONE, null);
-        MobclickAgent.reportError(this, "测试错误");
 
         String deviceInfo[] = UMConfigure.getTestDeviceInfo(this);
 
